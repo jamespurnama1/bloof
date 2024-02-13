f598e0b3f851b27ba019f443f8d8c3341cb741b9
jamespurnama1 <james.henry.purnama0@gmail.com>
2024-02-13 11:38:14 +0700

    fix menu recursive by upgrading nuxt improve menu UX
    TODO:
    page transition
    more animations

--- pages/menu.vue
+++ pages/menu.vue
@@ -45,17 +45,8 @@ onMounted(() => {
 </script>
 
 <template>
-  <Transition name="fade">
-    <div v-if="hint"
-      class="md:ml-[100px] top-0 left-0 w-full h-full fixed bg-white bg-opacity-75 flex items-center justify-center z-10 pointer-events-none">
-      <figure class="flex flex-col items-center justify-center">
-        <img src="/images/arrow.png" alt="Swipe right" class="-rotate-90 h-64 w-auto" />
-        <figcaption class="font-semibold text-3xl text-center">Swipe to browse the menu.</figcaption>
-      </figure>
-    </div>
-  </Transition>
-  <section class="md:ml-[100px] pt-24 md:pt-0 flex flex-col relative">
-    <div class="flex gap-2 px-4 py-4 md:px-12 md:py-8 flex-col">
+  <section class="md:ml-[100px] h-full pt-24 md:pt-0 flex flex-col relative">
+    <div class="flex relative gap-2 px-4 py-4 md:px-12 md:py-8 flex-col z-[15]">
       <h1 class="text-center md:text-left text-5xl md:text-7xl xl:text-9xl">Menu</h1>
       <div class="flex justify-center md:justify-start gap-2">
         <button @click="sel = 'food'" class="button_pink small hover:opacity-100"
@@ -63,17 +54,26 @@ onMounted(() => {
         <button @click="sel = 'drinks'" class="button_teal small hover:opacity-100"
           :class="[sel === 'drinks' ? '' : 'opacity-50']">drinks</button>
       </div>
-      <p class="text-xs text-center md:text-start">Click on menu to zoom in &amp; out.</p>
+      <p class="md:text-base text-xs text-center md:text-start">Click on menu to zoom in &amp; out.</p>
     </div>
-    <div class="invisible" />
-    <!-- <img class="w-full h-auto object-cover" src="/menu.jpg" alt="menu"> -->
+    <div class="invisible">
+    </div>
+    <Transition name="fade">
+      <div v-if="hint"
+        class="hint pt-60 md:pt-48 xl:pt-60 left-0 top-0 w-full h-full absolute bg-warm-200 bg-opacity-75 flex items-center justify-center z-10 pointer-events-none">
+        <figure class="flex flex-col items-center justify-center">
+          <img src="/images/arrow.png" alt="Swipe right" class="rotate-90 h-32 md:h-64 w-auto" />
+          <figcaption class="font-semibold text-lg md:text-3xl text-center">Swipe to browse the menu.</figcaption>
+        </figure>
+      </div>
+    </Transition>
     <ClientOnly v-if="CMSStore.drinks.length && CMSStore.food.length">
-      <flipbook @flip-left-start="hint = false" @flip-right-start="hint = false" :zooms="[1, 2]"
-        class="flipbook mx-auto md:w-2/3 w-full px-12 self-center absolute transition-opacity top-60 md:top-48 xl:top-60"
+      <flipbook @flip-left-start="hint = false" @flip-right-start="hint = false" :zooms="[1, 2]" :nPolygons="20"
+        class="flipbook w-full px-12 self-center absolute transition-opacity top-64"
         :class="[sel === 'food' ? 'opacity-100' : 'opacity-0 pointer-events-none']" :pages="CMSStore.food"
         :pagesHiRes="CMSStore.food_highres"></flipbook>
-      <flipbook @flip-left-start="hint = false" @flip-right-start="hint = false" :zooms="[1, 2]"
-        class="flipbook mx-auto md:w-2/3 w-full px-12 self-center absolute transition-opacity top-60 md:top-48 xl:top-60"
+      <flipbook @flip-left-start="hint = false" @flip-right-start="hint = false" :zooms="[1, 2]" :nPolygons="20"
+        class="flipbook w-full px-12 self-center absolute transition-opacity top-64"
         :class="[sel === 'drinks' ? 'opacity-100' : 'opacity-0 pointer-events-none']" :pages="CMSStore.drinks"
         :pagesHiRes="CMSStore.drinks_highres"></flipbook>
     </ClientOnly>
@@ -88,4 +88,5 @@ onMounted(() => {
   }
 
   height: calc((25.5 * (90vw - 32px)) / 18);
-}</style>
+}
+</style>
